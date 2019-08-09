5e473c37bcbe9b88213f926b225f1ee1c03aeaa9
simeneb <53920973+simeneb@users.noreply.github.com>
2019-08-09 23:51:31 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-_sys_info = pickle_module.load(f,encoding='latin1')
-unpickler = pickle_module.Unpickler(f,encoding='latin1')
+from functools import partial
+import pickle
+pickle.load = partial(pickle.load, encoding="latin1")
+pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
+#_sys_info = pickle_module.load(f,encoding='latin1')
+#unpickler = pickle_module.Unpickler(f,encoding='latin1')
 
 export_file_url = 'https://drive.google.com/uc?export=download&id=1OyNhmKilO2X1BXTkKO13pJeHHWygyanq'
 export_file_name = 'export.pkl'
